adfa5c66ddb0fdce72fabbe7d6269ac38a5e1813
Succuruddi <sbelmy03@gmail.com>
2022-08-20 15:29:07 +0200

    Fine settimana

--- config/cron-task.js
+++ config/cron-task.js
@@ -1,6 +1,19 @@
 module.exports = {  
   '*/1,2,3,4,5,6,7,8,9,10 * * * *': async ({ strapi }) => {
     console.log('cron running');
-    strapi.service('api::pub.pub').createTweet();
+    //strapi.service('api::pub.pub').createTweet();
+   
+   
+    let event = await strapi.entityService.findOne('api::event.event',1 ,{ // predno un evento dopo passo alla funzione create tweet descrizione titolo
+     
+    });
+
+    console.log(event);
+    
+    var verifica= event.description;
+    strapi.service('api::event.event').createTweet(verifica);
+   
+   
+    ;  
 
 }}
--- src/api/event/services/event.js
+++ src/api/event/services/event.js
@@ -7,8 +7,9 @@ const {
   createCoreService
 } = require('@strapi/strapi').factories;
 const {
-  Client,
-  auth
+  Client
+ // ,auth    l'ho cambito io percghè sull'altro file non c'era auth
+
 } = require('twitter-api-sdk');
 
 //import got from 'got';
@@ -212,8 +213,74 @@ integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52n
     return htmlResult;
   },
 
-  async createTweet() {
-    console.log("hello");
+  async createTweet(description) {
+
+    const oauth = OAuth({
+
+      consumer: {
+
+        key: 'P2aTclntqMENhz7wDUnVZz822',
+
+        secret: 'zpcYTn8y6WiHTQ8rFHP3na4OetmgMLBnOZW8aNZxGeMcikE7LK'
+
+      },
+
+      signature_method: 'HMAC-SHA1',
+
+      hash_function: (baseString, key) => crypto.createHmac('sha1', key).update(baseString).digest('base64')
+
+    });
+
+
+
+    const token = {
+
+      key: '1555201215352504320-m8mrgpbn1GI17ej1YBInanDQTG7yVp',
+
+      secret: '5VLtHB967vlEyPHLNtD4fm2iNVQJMG7q4Ie4bhDN0w3fA',
+
+    };
+
+    const authHeader = oauth.toHeader(oauth.authorize({
+
+      url: 'https://api.twitter.com/2/tweets',
+
+      method: 'POST'
+
+    }, token));
+
+
+
+    try {
+
+
+
+      const req = await axios.post('https://api.twitter.com/2/tweets', {
+
+        "text": description
+
+      }, {
+
+        headers: {
+
+          Authorization: authHeader["Authorization"],
+
+          'user-agent': "v2CreateTweetJS",
+
+          'content-type': "application/json",
+
+          'accept': "application/json"
+
+        }
+
+      });
+
+    } catch (error) {
+
+      console.log(error);
+
+    }
+
   },
 
   async createTweet2() {
