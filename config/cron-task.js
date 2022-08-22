7af7285747bc3b587ddd35319810cc020f062d3a
Pablo Aldana <thorinroble@gmail.com>
2022-08-22 11:54:08 +0200

    adding populate. change findOne to findMany so that it rerieves all events, this will need a filter tho, and adding controller in case there is no event

--- config/cron-task.js
+++ config/cron-task.js
@@ -1,19 +1,22 @@
-module.exports = {  
-  '*/1,2,3,4,5,6,7,8,9,10 * * * *': async ({ strapi }) => {
+module.exports = {
+  '*/1,2,3,4,5,6,7,8,9,10 * * * *': async ({
+    strapi
+  }) => {
     console.log('cron running');
     //strapi.service('api::pub.pub').createTweet();
-   
-   
-    let event = await strapi.entityService.findOne('api::event.event',1 ,{ // predno un evento dopo passo alla funzione create tweet descrizione titolo
-     
+
+
+    let event = await strapi.entityService.findMany('api::event.event', { // predno un evento dopo passo alla funzione create tweet descrizione titolo
+      populate: ['organisers', 'performers']
     });
 
     console.log(event);
-    
-    var verifica= event.description;
-    strapi.service('api::event.event').createTweet(verifica);
-   
-   
-    ;  
+    if (event != null && event.length > 0) {
+      var verifica = event[0].description;
+      strapi.service('api::event.event').createTweet(verifica);
+    }
 
-}}
+    ;
+
+  }
+}
