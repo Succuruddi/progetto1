b27107b76f7699d2dcd9362c2c9d7f86bbc9d8ac
Succuruddi <sbelmy03@gmail.com>
2022-08-24 14:35:16 +0200

    delay

--- config/cron-task.js
+++ config/cron-task.js
@@ -1,12 +1,12 @@
 module.exports = {
-  '*/1,2,3,4,5,6,7,8,9,10 * * * *': async ({
+  '*/1,2,3,4,5,6,7,8,9,10 * * * *': async ({ // '*/29 */2,8,14,20 * * *'
     strapi
   }) => {
     console.log('cron running');
     //strapi.service('api::pub.pub').createTweet();
 
     var startDate=new Date();
-    
+    startDate.setDate(startDate.getHours()+2);
     var endDate= new Date();
     endDate.setHours(startDate.getHours()+6); //imposto il lasso di tempo
      
